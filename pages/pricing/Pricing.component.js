4d83d7f38001007588f765280ba1c3f590831f79
Andrew Crescencio <andrew_workstation@hotmail.com>
2022-05-19 15:04:58 -0300

    fix: pricing page blur

--- pages/pricing/Pricing.component.js
+++ pages/pricing/Pricing.component.js
@@ -1,3 +1,34 @@
 export default {
   name: 'PagesPricing',
+  data() {
+    return {
+      width: 0,
+    }
+  },
+  mounted() {
+    window.addEventListener('resize', this.getDimensions)
+  },
+  computed: {
+    updatewidth() {
+      if (process.client) {
+        const width = window.innerWidth
+        return width
+      } else {
+        return null
+      }
+    },
+    blurUrl() {
+      if (process.client) {
+        const mobileUrl = 'finsweet-blur-pricing-mobile'
+        const desktopUrl = 'finsweet-blur-pricing-desktop.webp'
+        const width = this.width
+        return width > 1280 ? desktopUrl : mobileUrl
+      }
+    },
+  },
+  methods: {
+    getDimensions() {
+      this.width = window.innerWidth
+    },
+  },
 }
